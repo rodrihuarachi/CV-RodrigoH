cf6310cfe1487692199366790bcd57186221dd08
rodrihuarachi <rodrihuarachi@icloud.com>
2024-11-08 11:01:53 -0300

    Changes in data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,268 +20,146 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Rodrigo Huarachi R.",
+  initials: "RH",
+  location: "Buenos Aires, Argentina",
+  locationLink: "https://www.google.com/maps/place/Buenos+Aires/",
   about:
-    "Detail-oriented Full Stack Engineer dedicated to building high-quality products.",
+    "Data Engineer and Data Analyst",
   summary:
-    "As a Full Stack Engineer, I specialize in taking products from concept to launch. I excel in leading teams and creating environments where individuals perform at their best. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience working remotely with companies worldwide.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "I'm a dedicated Data Analyst and Data Engineer with over 2 years of experience delivering innovative, client-focused solutions. I strive to provide accurate and valuable results, working closely with clients to understand their needs and guide them toward achieving their goals. My commitment goes beyond simply addressing analytical challenges; I build comprehensive solutions that enhance data management and utilization. I'm always driven to exceed expectations, ensuring efficient, transparent service tailored to the unique demands of each project.",
+  avatarUrl: "https://avatars.githubusercontent.com/u/69877961?v=4",
+  personalWebsiteUrl: "",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "rodrihuarachi@gmail.com",
+    tel: "+5491159439878",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/rodrihuarachi",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/rodrigohuarachi/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/rodrihuarachi",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Henry Bootcamp",
+      degree: "Data Scientist",
+      start: "2023",
+      end: "2023",
+    },
+    {
+      school: "E.E.S.T. John F. Kennedy",
+      degree: "Programming Technician",
+      start: "2011",
+      end: "2017",
     },
   ],
   work: [
     {
-      company: "Film.io",
-      link: "https://film.io",
+      company: "CoreBI",
+      link: "https://corebi.com.ar/",
       badges: ["Remote"],
-      title: "Software Architect",
+      title: "Data Engineer",
       logo: ConsultlyLogo,
       start: "2024",
       end: null,
       description:
-        "Leading the development of the Film.io platform. Technologies: React, TypeScript, Node.js",
+        "Client: Pan American Energy \
+        Data Warehouse Monitoring, including: \
+        • Job and Packages control through SQL Server. \
+        • Data Analysis using Excel and Power BI. \
+        • Presentation of monthly executive report. \
+        • Smooth client and the team communication.",
     },
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      company: "HUARA Muebles",
+      link: "",
+      badges: ["In Person"],
+      title: "Financial Management",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "2022",
+      end: "2023",
       description:
-        "Implemented new features, led a squad, improved code delivery process, and initiated migration from Emotion to Tailwind CSS. Technologies: React, TypeScript, GraphQL",
+        "• I developed a system in Excel to manage income, expenses, salaries and production expenses. \
+        •  Created annual financial visualization highlighting earnings, debt and key trends. \
+        •  Design a tool that automatically adjusts furniture prices based on changes. \
+        •  I implemented a budget system in Excel with material price tables, automatic calculations and printing options.",
     },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, and AAA. Built a live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Led the Android team and developed apps for major Polish companies like LOT, Polskie Radio, Agora, and PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Developed and tested software for LTE base stations",
-    },
+    
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Python",
+    "SQL",
+    "Git",
+    "Docker",
+    "Big Query",
+    "Airflow",
+    "Power BI",
+    "Power Automate",
+    "SSIS"
   ],
   projects: [
     {
-      title: "Parabol",
+      title: "Migratory Flows",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Data Engineer",
+        "Python",
+        "Cloud Storage",
+        "Cloud Functions",
+        "BigQuery",
+        "Power BI",
       ],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
+        "Development of a data solution (Data Pipeline, Database, Data Warehouse, Dashboard, Predictive Models, etc.) to be able to effectively communicate the relationship of human migrations with the different socioeconomic and geopolitical aspects. ",
       logo: ParabolLogo,
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "migratory-flows.com",
+        href: "hhttps://final-project-page.vercel.app/",
       },
     },
     {
-      title: "Evercast",
+      title: "MLOPs with Steam Games",
       techStack: [
-        "Lead Frontend Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Data Scientist",
+        "ETL",
+        "Python",
+        "FastAPI",
+        "Render",
       ],
       description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
+        "Development of an API in the cloud and application of 2 Machine Learning models: Sentiment Analysis for user comments and Game Recommendation based on a specific game.",
       logo: EvercastLogo,
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "mlops-steam-games.com",
+        href: "https://deploy-pi-rodrigo-huarachi.onrender.com/",
       },
     },
     {
-      title: "Consultly",
+      title: "IT Help Desk",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Data Analyst",
+        "Python",
+        "Power BI",
+        "Excel",
+        "DAX",
       ],
-      description: "A platform to build and grow your online business",
+      description: "Development of a Power BI dashboard analyzing IT support tickets",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "powerbi-it-helpdesk.com",
+        href: "https://app.powerbi.com/view?r=eyJrIjoiNjhmZjg1MzYtYTMyYS00OGM0LTk3YjktMTZiMTkxNGU4YzNhIiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9",
       },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "Personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers, and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description: "Howdy is a place for joining communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+    }
   ],
 } as const;
